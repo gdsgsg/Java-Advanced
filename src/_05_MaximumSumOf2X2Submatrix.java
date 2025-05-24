92564f6c450567f03ff19ce992beef315e6e0fe3
Vasil.Vasilev <vasilvasilev948@gmail.com>
2025-05-24 18:05:57 +0300

    cleanup

--- src/_05_MaximumSumOf2X2Submatrix.java
+++ src/_05_MaximumSumOf2X2Submatrix.java
@@ -1,45 +1,59 @@
 import java.util.Arrays;
 import java.util.Scanner;
+
 public class _05_MaximumSumOf2X2Submatrix {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         int[] array = Arrays.stream(scanner.nextLine().
-                split(", ")).
+                        split(", ")).
                 mapToInt(Integer::parseInt).
                 toArray();
 
 
+        int[][] matrix = new int[array[0]][array[1]];
+        int[][] squareMatrix = new int[2][2];
+        populateMatrix(scanner, matrix);
+        int sum = 0;
+        for (int i = 0; i < matrix.length - 1; i++) {
 
-        int [][] matrix = new int[array[0]][array[1]];
-        populateMatrix(scanner,matrix);
-        int [][] squareMatrix = new int [2][2];
-        int [][] squareMatrix2 = new int [2][2];
+            for (int j = 0; j < matrix[0].length - 1; j++) {
 
-        for (int i = 0; i <= matrix.length ; i++) {
-            for (int j = 0; j <= matrix[0].length ; j++) {
-                squareMatrix[0][j] = matrix[i][j];
+                //i                -
+                //7, 1, 3, 3, 2, 1 -
+                //1, 3, 9, 8, 5, 6 -
+                //4, 6, 7, 9, 1, 0 -
+                int central = matrix[i][j];
+                int toTheRight = matrix[i][j + 1];
+                int diagonalDown = matrix[i + 1][j + 1];
+                int downFromCentral = matrix[i + 1][j];
+                int tempSum = central + toTheRight + diagonalDown + downFromCentral;
+
+                if (tempSum > sum) {
+                    sum = tempSum;
+                    squareMatrix[0][0] = central;
+                    squareMatrix[0][1] = toTheRight;
+                    squareMatrix[1][1] = diagonalDown;
+                    squareMatrix[1][0] = downFromCentral;
+                }
+
 
             }
 
         }
 
-
-            //i
-            //7, 1, 3, 3, 2, 1
-            //1, 3, 9, 8, 5, 6
-            //4, 6, 7, 9, 1, 0
-
-
+        System.out.println(squareMatrix[0][0] + " " + squareMatrix[0][1]);
+        System.out.println(squareMatrix[1][0] + " " + squareMatrix[1][1]);
+        System.out.println(sum);
 
     }
 
-    private static int[][] populateMatrix (Scanner scanner, int[][]matrix){
+    private static int[][] populateMatrix(Scanner scanner, int[][] matrix) {
 
-        for (int i = 0; i < matrix.length ; i++) {
+        for (int i = 0; i < matrix.length; i++) {
 
-            int [] tempArray = Arrays.stream(scanner.nextLine().
-                    split(", ")).
+            int[] tempArray = Arrays.stream(scanner.nextLine().
+                            split(", ")).
                     mapToInt(Integer::parseInt).
                     toArray();
 
